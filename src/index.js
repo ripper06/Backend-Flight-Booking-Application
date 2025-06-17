f1d721da77d3232b8f3da7dba48f223384923922
ripper06 <jyotiranjan.pvt@gmail.com>
2025-06-17 11:40:44 +0530

    Saving my local changes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,27 @@ app.use('/api',apiRoutes);
 
 
 
-app.listen(ServerConfig.PORT,()=>{
-    console.log(`Successfully server started on port : ${ServerConfig.PORT}`);
+app.listen(ServerConfig.PORT, async()=>{
+    console.log(`Successfully server started on port : ${ServerConfig.PORT}`); 
+    
+    //ADDITIONAL FEATURES GIVEN BY SEQELIZE
+
+    const {City,Airport} = require('./models');
+    const delhi = await City.findByPk(1);
+    console.log(delhi);
+
+    //const airport = await Airport.create({name : "Indira Gandhi International Airport", code : "DEL", cityId : 1});
+    //const Del = await delhi.createAirport({name : "Indira Gandhi International Airport", code:"DEL"});
+    // console.log(safdarjung);
+
+    // const airportsinDel = await delhi.getAirports();
+    // console.log(airportsinDel);
+
+    // const VIDDairport = await Airport.findByPk(7);
+    // await delhi.removeAirport(VIDDairport);
+    await City.destroy({
+        where : {
+            id : 1
+        }
+    })
 });
--- src/models/airport.js
+++ src/models/airport.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define association her
+      this.belongsTo(models.City, {
+        foreignKey : 'cityId',
+        onDelete : 'CASCADE',
+        onUpdate : 'CASCADE',
+      });
     }
   }
   Airport.init({
