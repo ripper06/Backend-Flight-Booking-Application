7248a11d6731adc776ca63ae243d630ed7117fd7
ripper06 <jyotiranjan.pvt@gmail.com>
2025-06-26 11:15:06 +0530

    added seats model in our db associated to airplane

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -12,7 +12,13 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.hasMany(models.Flight,{
-        foreignKey: 'airplaneId'
+        foreignKey: 'airplaneId',
+        onDelete : 'CASCADE'
+      });
+
+      this.hasMany(models.Seat,{
+        foreignKey: 'airplaneId',
+        onDelete : 'CASCADE'
       })
     }
   }
