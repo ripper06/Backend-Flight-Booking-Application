e9a414440e9caaa5fc99de74b186089b3af0a75a
ripper06 <jyotiranjan.pvt@gmail.com>
2025-08-14 11:03:23 +0530

    Changed the routes to /airplanesService/api/v1/...

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api',apiRoutes);
+app.use('/flightsService/api',apiRoutes);
 
 
 const PORT = ServerConfig.PORT || 3000;
@@ -19,25 +20,4 @@ const PORT = ServerConfig.PORT || 3000;
 
 app.listen(PORT, async()=>{
     console.log(`Successfully server started on port : ${PORT}`); 
-    
-    //ADDITIONAL FEATURES GIVEN BY SEQELIZE
-
-    // const {City,Airport} = require('./models');
-    // const delhi = await City.findByPk(1);
-    // console.log(delhi);
-
-    //const airport = await Airport.create({name : "Indira Gandhi International Airport", code : "DEL", cityId : 1});
-    //const Del = await delhi.createAirport({name : "Indira Gandhi International Airport", code:"DEL"});
-    // console.log(safdarjung);
-
-    // const airportsinDel = await delhi.getAirports();
-    // console.log(airportsinDel);
-
-    // const VIDDairport = await Airport.findByPk(7);
-    // await delhi.removeAirport(VIDDairport);
-    // await City.destroy({
-    //     where : {
-    //         id : 1
-    //     }
-    // })
 });
